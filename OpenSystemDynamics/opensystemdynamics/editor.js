704a1d41c0e7030f7ed0c0ec49d12a1203e514f6
Unknown <magnus.ja.gustafsson@gmail.com>
2018-07-01 17:23:51 +0200

    Removed commented code

--- OpenSystemDynamics/opensystemdynamics/editor.js
+++ OpenSystemDynamics/opensystemdynamics/editor.js
@@ -1954,10 +1954,6 @@ class RiverVisual extends BaseConnection {
 		// Get start position from attach
 		// start_anchor is null if we are currently creating the connection
 		// start_attach is null if we are not attached to anything
-
-		// if (this.anchorPoints.length < 2) { 
-		// 	return; 
-		// }
 		
 		let points = this.getPathPoints();
 		let connectionStartPos = points[1];
