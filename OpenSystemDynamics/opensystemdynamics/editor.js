4f2ea62b2caff58e074e20d2bf7dd3252c76b336
Unknown <magnus.ja.gustafsson@gmail.com>
2018-07-03 18:33:51 +0200

    Added isChildSelected() method in LinkVisual

--- OpenSystemDynamics/opensystemdynamics/editor.js
+++ OpenSystemDynamics/opensystemdynamics/editor.js
@@ -2742,6 +2742,19 @@ class LinkVisual extends BaseConnection{
 		this.afterAnchorUpdate(anchorTypeEnum.bezier1);
 		this.afterAnchorUpdate(anchorTypeEnum.bezier2);	
 	}
+	isChildSelected() {
+		let children = getChildren(this.id);
+		let suffixs = [".start_anchor", ".b1_anchor", ".b2_anchor", ".end_anchor"];
+		let selectedArray = [];
+		for (i = 0; i < suffixs.length; i++) {
+			let suffix = suffixs[i];
+			let child = children[this.id+suffix];
+			if (child) {
+				selectedArray.push(child.selected);	
+			}
+		}
+		return selectedArray.includes(true);
+	}
 	update() {
 		// This function is similar to TwoPointer::update but it takes attachments into account
 		
@@ -2751,7 +2764,7 @@ class LinkVisual extends BaseConnection{
 		
 		//console.log(this.b1_anchor.get_pos());
 		//let connectionCenter = this.b1_anchor.get_pos();
-		
+
 		if (this.start_attach != null && this.start_anchor != null) {
 			if (this.start_attach.get_pos) {
 				let oldPos = this.start_anchor.get_pos();
@@ -2773,6 +2786,10 @@ class LinkVisual extends BaseConnection{
 			}
 		}
 		super.update();
+
+		// if (! this.isChildSelected() && (this.end_attach == null || this.start_attach == null)) {
+		// 	console.log(`Link with id ${this.id} should self delete`);
+		// }
 	}
 }
 
