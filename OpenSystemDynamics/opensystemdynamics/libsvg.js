0b9ebc8b3b73d6654262e598e461eb6a19c06bc2
Unknown <magnus.ja.gustafsson@gmail.com>
2018-07-05 12:33:07 +0200

    Fixed faulty attaches to invisible couds by moving cloud out of sight

--- OpenSystemDynamics/opensystemdynamics/libsvg.js
+++ OpenSystemDynamics/opensystemdynamics/libsvg.js
@@ -354,8 +354,10 @@ function svgCloud(stroke, fill) {
 	newElement.setAttribute("stroke-width", "1");
 	newElement.setAttribute("fill", fill);
 	newElement.setAttribute("d", "m -0.8447564,-11.14014 c -4.6214865,0.0079 -8.5150638,3.4528784 -9.0815386,8.0394981 -2.433142,0.4797384 -4.187489,2.61298232 -4.188373,5.0929775 -6.93e-4,2.8681392 2.323935,5.1936858 5.1920483,5.1941646 H 7.671332 C 11.368943,7.1872852 14.36665,4.1896043 14.365861,0.49198425 14.223787,-3.916487 10.814437,-6.550028 7.2876342,-6.1810461 5.7167742,-9.2242012 2.5799338,-11.137323 -0.84475524,-11.140887 Z");
-	this.visibility = true;
+	newElement.visibility = true;
 	newElement.pos = [0, 0];
+	newElement.defaultStroke = stroke;
+	newElement.defaultFill = fill;
 
 	newElement.setPos = function (pos, adjecentPos) {
 		let offset = [0,0];
@@ -381,12 +383,13 @@ function svgCloud(stroke, fill) {
 			this.setAttribute("transform","translate("+this.pos[0]+","+this.pos[1]+")");
 		} else {
 			// setting visibility to "hidden" causes bugs, like clicking on invisible objects
-			this.setAttribute("transform","translate("+this.pos[0]+","+this.pos[1]+") scale(0)");
+			// Therefore I just place the object outside the screen
+			this.setAttribute("transform","translate(-20, 20)");
 		}
 	}
 
 	newElement.setVisibility = function (isVisible) {
-		this.visible = isVisible;
+		this.visible = isVisible;		
 	}
 	
 	svgplane.appendChild(newElement);
