f859d35448000a7378e3e57459a829230f762422
Erik Gustafsson <ekir.gustafsson@gmail.com>
2019-12-02 19:16:37 +0100

    Worked on licenses

--- OpenSystemDynamics/src/libsvg.js
+++ OpenSystemDynamics/src/libsvg.js
@@ -1,12 +1,13 @@
 /*
 
-Copyright 2010-2019 StochSD-Team. All rights reserved.
+Copyright 2010-2019 StochSD-Team and Scott Fortmann-Roe. All rights reserved.
 
 This file may distributed and/or modified under the
 terms of the Affero General Public License (http://www.gnu.org/licenses/agpl-3.0.html).
 
-*/
+The Insight Maker Engine was contributed to StochSD project from Insight Maker project by Scott Fortmann-Roe, http://insightmaker.com
 
+*/
 function svg_from_string_with_group(instring) {
 	  var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'g'); //Create a path in SVG's namespace
 	  newElement.innerHTML=instring;
