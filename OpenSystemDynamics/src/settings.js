99c0b4f7578245183df123f3ccd978a678d9de09
Erik Gustafsson <ekir.gustafsson@gmail.com>
2019-12-03 18:25:30 +0100

    Add new window dialog

--- OpenSystemDynamics/src/settings.js
+++ OpenSystemDynamics/src/settings.js
@@ -2,6 +2,8 @@ var Settings = {
 	fileExtension: ".ssd",
 	// Controls the zoom in NW mode
 	nwInitZoom: 0.5,
+	// Whatever to use New Model Window
+	newModelWindow: true, 
 	// The font size under the primitives
 	primitiveFontSize: 13,
 	// Whatever to show debug
