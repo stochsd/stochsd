c5c5e3a2779a8bf1845cf1ed372cab2d459517da
StochSD Team <nope>
2019-12-04 15:56:25 +0100

    header

--- OpenSystemDynamics/src/insightmaker/SimulationEngine/SimpleCalc.js
+++ OpenSystemDynamics/src/insightmaker/SimulationEngine/SimpleCalc.js
@@ -1,12 +1,10 @@
 "use strict";
 /*
 
-Copyright 2010-2019 Scott Fortmann-Roe. All rights reserved.
+Copyright 2010-2015 Scott Fortmann-Roe. All rights reserved.
 
 This file may distributed and/or modified under the
-terms of the Affero General Public License (http://www.gnu.org/licenses/agpl-3.0.html).
-
-The Insight Maker Engine was contributed to StochSD project from Insight Maker project by Scott Fortmann-Roe, http://insightmaker.com
+terms of the Insight Maker Public License (https://InsightMaker.com/impl).
 
 */
 
