076a3af0442ba4bb48f1e8ecad083ca1435acb75
StochSD Team <nope>
2019-12-06 19:57:21 +0100

    Improved build script

--- distribute/build-for-linux/build-for-linux.sh
+++ distribute/build-for-linux/build-for-linux.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # A dependency for running this script is to build the javascript output to output/ by running build.sh
+set -x
 # Version v0.32.4 is well tested on Ubuntu 18.04. Don't update without testing
+
+# Work in a temporary directory
+mkdir tmp
+cd tmp
 
 RELEASE_NAME="stochsd-2019.12.06-linux"
 cd $(dirname $0)
@@ -13,5 +18,7 @@ rm -Rf stochsd
 tar -xzvf nwjs-sdk-v0.32.4-linux-x64.tar.gz
 mv nwjs-sdk-v0.32.4-linux-x64 $RELEASE_NAME
 cd $RELEASE_NAME
-cp -av ../output/package.nw stochsd/package.nw
+cp -av ../../../output/package.nw package.nw
+mv nw stochsd
+cd ..
 tar -czvf $RELEASE_NAME.tar.gz $RELEASE_NAME/
