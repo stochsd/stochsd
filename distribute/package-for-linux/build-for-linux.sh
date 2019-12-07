84a98df599f36b48c92a02956f988b24e09ee4bf
StochSD-Team <[]>
2019-12-07 23:07:25 +0100

    Update linux build script

--- distribute/package-for-linux/build-for-linux.sh
+++ distribute/package-for-linux/build-for-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A dependency for running this script is to build the javascript output to output/ by running build.sh
-set -x
+set -ex
 # Version v0.32.4 is well tested on Ubuntu 18.04. Don't update without testing
 
 if ! test -d "../output/package.nw"; then
@@ -12,7 +12,7 @@ STOCHSD_VERSION=$(node ../get-stochsd-version.js)
 echo "Building stochsd version "$STOCHSD_VERSION
 
 # Work in a temporary directory
-mkdir tmp
+mkdir -p tmp
 cp md5sum.nwjs-linux tmp/
 cd tmp
 
@@ -25,11 +25,13 @@ then
 fi
 rm -Rf stochsd
 tar -xzvf nwjs-sdk-v0.32.4-linux-x64.tar.gz
+rm -Rf $RELEASE_NAME
 mv nwjs-sdk-v0.32.4-linux-x64 $RELEASE_NAME
 cd $RELEASE_NAME
 cp -av ../../../output/package.nw package.nw
 cp ../../../../OpenSystemDynamics/src/license.html .
 cp ../../../../OpenSystemDynamics/src/third-party-licenses.html .
+pwd
 mv nw stochsd
 cd ..
 tar -czvf $RELEASE_NAME.tar.gz $RELEASE_NAME/
