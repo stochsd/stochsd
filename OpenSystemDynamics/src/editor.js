388422870656032664819821ad7747131f36667c
Unknown <magnus.ja.gustafsson@gmail.com>
2020-06-01 18:28:14 +0200

    Remove old code

--- OpenSystemDynamics/src/editor.js
+++ OpenSystemDynamics/src/editor.js
@@ -6034,16 +6034,10 @@ function syncVisual(tprimitive) {
 			if (tprimitive.source != null) {
 				// Attach to object
 				connection.setStartAttach(get_object(tprimitive.source.getAttribute("id")));
-			} else {
-				// Set UI-coordinates to coordinates in primitive
-				connection.start_anchor.set_pos(source_position);
 			}
 			if (tprimitive.target != null) {
 				// Attach to object
 				connection.setEndAttach(get_object(tprimitive.target.getAttribute("id")));
-			} else {
-				// Set UI-coordinates to coordinates in primitive
-				connection.end_anchor.set_pos(target_position);
 			}
 			let bezierPoints = [
 				tprimitive.getAttribute("b1x"),
