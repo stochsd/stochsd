25f9e8e1d26b94cae02626fca4c84add8b068ab5
Unknown <magnus.ja.gustafsson@gmail.com>
2020-06-17 13:00:37 +0200

    Fix when printing diagram month shows correct date now
    new Date().getMonth() returns value 0 to 11

--- OpenSystemDynamics/src/editor.js
+++ OpenSystemDynamics/src/editor.js
@@ -6241,7 +6241,7 @@ function printDiagram() {
 	let fileName = fileManager.fileName;
 	
 	let d = new Date();
-	let month = d.getMonth() < 10 ? `0${d.getMonth()}` : d.getMonth();
+	let month = d.getMonth()+1 < 10 ? `0${d.getMonth()+1}` : d.getMonth()+1;
 	let day = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate();
 	let hours = d.getHours() < 10 ? `0${d.getHours()}` : d.getHours();
 	let minutes = d.getMinutes() < 10 ? `0${d.getMinutes()}` : `${d.getMinutes()}`;
