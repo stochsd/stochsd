70718625764215e45ada00805405c4815dd1c5d2
Unknown <magnus.ja.gustafsson@gmail.com>
2020-06-20 18:46:29 +0200

    Add format_number can use options.show_plus_sign

--- OpenSystemDynamics/src/format_number.js
+++ OpenSystemDynamics/src/format_number.js
@@ -19,48 +19,79 @@ function round_exponent(exponent) {
     return Math.floor(exponent/3)*3;
 }
 
-/*
-    {
-        decimals: int,
-        precision: int, // also called sigificant digits 
-        round_to_zero_limit, float,
-        use_e_format_upper_limit: float,
-        use_e_format_lower_limit: float,
-        show_plus_sign: bool 
-    }
-*/
 
-function format_number(value, options) {
+let default_options = {
+    decimals: undefined,
+    precision: undefined, // also called sigificant digits 
+    round_to_zero_limit: undefined,
+    use_e_format_upper_limit: 1e8, 
+    use_e_format_lower_limit: 1e-6, 
+    show_plus_sign: false
+}
+
+function format_number(value, options = {}) {
+    for (let key of Object.keys(default_options)) {
+        if(options[key] === undefined) {
+            options[key] = default_options[key];
+        }
+    }
+
     if(value === 0) {
         return "0";
     }
     let log = Math.log10(Math.abs(value));
     let exponent = Math.floor(log);
     let isPositive = value >= 0;
-    let roundedExponent = round_exponent(exponent);
 
-    let coeffStr = "";
+    // exponent rounded of to nerest 3 divisible number: ...-6, -3, 0, 3, 6,...
+    let exp3 = round_exponent(exponent);
+
+    selected_exp = 0;
+    if ((Math.abs(value) >= options.use_e_format_upper_limit) ||
+        (Math.abs(value) <= options.use_e_format_lower_limit)) {
+        selected_exp = exp3;
+    }
+    
+
+    let digits_above = "";
+    let digits_below = "";
     let rest = Math.abs(value);
-    let dotIsSet = false;
-    for (let i = roundedExponent; i > roundedExponent-10; i--) {
+    for (let i = selected_exp; i > selected_exp-10; i--) {
         tmpNum = Math.floor(rest/(10**i));
-        coeffStr += `${tmpNum}`;
+        if (i === selected_exp) {
+            // NOTE: for numbers above 1e21 this will turn into e-format by the tostring method
+            digits_above += `${tmpNum}`;
+        } else {
+            digits_below += `${tmpNum}`;
+        }
         rest -= tmpNum*10**i;
-        if (dotIsSet === false) {
-            coeffStr += ".";
-            dotIsSet = true;
+    }
+    
+    let sign_str = options.show_plus_sign ? "+" : "";
+    if (! isPositive) {
+        sign_str = "-";
+    }
+
+    // determine decimals/precision
+    if (! isNaN(options.decimals)) {
+        digits_below = digits_below.substr(0, options.decimals);
+        // -- round last digit here ---
+    } else if (! isNaN(options.precision)) {
+        digits_below = digits_below.substr(0, options.precision-digits_above.length);
+        // -- round last digit here ---
+    } else {
+        // default: clean up zeros
+        while(digits_below[digits_below.length-1] === "0") {
+            digits_below = digits_below.substr(0, digits_below.length-1);
         }
     }
-    if (! isPositive) {
-        coeffStr = `-${coeffStr}`;
+    let result = `${sign_str}${digits_above}`;
+    if (digits_below.length !== 0) {
+        result += `.${digits_below}`;
     }
-    // clean up zeros
-    while(coeffStr[coeffStr.length-1] === "0") {
-        coeffStr = coeffStr.substr(0, coeffStr.length-1);
-    }
-    let result = coeffStr;
-    if (roundedExponent !== 0) {
-        result = `${coeffStr}e${Math.sign(roundedExponent) === 1 ? "+": "-" }${Math.abs(roundedExponent)}`;
+    
+    if (selected_exp !== 0) {
+        result = `${result}e${Math.sign(selected_exp) === 1 ? "+": "-" }${Math.abs(selected_exp)}`;
     }
     return result;
 }
