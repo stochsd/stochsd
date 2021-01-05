7b3482929f3c0f706666cd4b082bb63ae216cd88
StochSD Team <>
2021-01-05 20:31:14 +0100

    package for Mac updated

--- distribute/package-for-mac/package-for-mac.sh
+++ distribute/package-for-mac/package-for-mac.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -ex
 
-NWJS_URL="https://dl.nwjs.io/v0.43.3/nwjs-sdk-v0.43.3-osx-x64.zip"
+# get NWJS_URL
+source url-config.sh
+
 NWJS_ZIP_NAME=$(basename $NWJS_URL)
-NWJS_ZIP_MD5=8c64b83ec6c6d045f2a00f0f1e2e59ce
 NWJS_FOLDER_NAME=$(basename $NWJS_ZIP_NAME .zip)
 
 STOCHSD_VERSION=$(node ../get-stochsd-version.js)
 echo "Building stochsd version "$STOCHSD_VERSION
 
-rm -Rf output
-
-mkdir -p download
-mkdir -p output
 
 cd download
 
@@ -20,18 +17,10 @@ cd download
 # If file does not exist
 if [[ ! -f $NWJS_ZIP_NAME ]]
 then
-  curl -O $NWJS_URL
+  echo "nwjs zip-file not found"
+  exit 1
 fi
 
-# If file has invalid md5
-if [[ "$(md5 -q $NWJS_ZIP_NAME)" != $NWJS_ZIP_MD5 ]]
-then
-	echo "Invalid md5. Redownloading"
-	curl -O $NWJS_URL
-fi
-
-#md5 -q nwjs-sdk-v0.43.3-osx-x64.zip
-unzip $NWJS_ZIP_NAME -d ../output
 cd ../output/
 cd $NWJS_FOLDER_NAME
 
