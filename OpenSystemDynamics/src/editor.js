efee5bac6285e449846cabd313734492d21c55c9
Magnus <magnus.ja.gustafsson@gmail.com>
2021-03-05 12:03:28 +0100

    Change StochSD Licence to always include current year.

--- OpenSystemDynamics/src/editor.js
+++ OpenSystemDynamics/src/editor.js
@@ -9608,9 +9608,10 @@ class LicenseDialog extends CloseDialog {
 		super();
 		this.setTitle("StochSD License");
 		
+		let currentYear = new Date().getFullYear();
 		this.setHtml(`
 		<p style="display: inline-block">
-		Copyright 2010-2019 StochSD-Team and Scott Fortmann-Roe. All rights reserved.<br/>
+		Copyright 2010-${currentYear} StochSD-Team and Scott Fortmann-Roe. All rights reserved.<br/>
 
 		The Insight Maker Engine was contributed to StochSD project from
 		Insight Maker project by Scott Fortmann-Roe, <a target="_blank" href="https://insightmaker.com">https://Insightmaker.com<a><br/>
