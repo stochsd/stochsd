25ea3e111d0dab31a97edd1ebd3ac4a9218e89bb
Magnus <magnus.ja.gustafsson@gmail.com>
2021-03-11 18:00:53 +0100

    Change add timeout logic to run updateChart less often when rescaling
    This is updates the graph frame more smooth but the plot/chart lags a little bit since it is not updated as often.

--- OpenSystemDynamics/src/editor.js
+++ OpenSystemDynamics/src/editor.js
@@ -2652,7 +2652,14 @@ class PlotVisual extends HtmlOverlayTwoPointer {
 		if (oldWidth !== newWidth || oldHeight !== newHeight) {
 			this.chartDiv.style.width = newWidth;
 			this.chartDiv.style.height = newHeight;
-			this.updateChart();
+
+			// Clear updating chart so only the last updateGraphics updates chart
+			// This limits the number of times updateCharts runs (updateChart is an expensive call)
+			if (this.updateChartTimeOut) {
+				clearTimeout(this.updateChartTimeOut);
+				this.updateChartTimeOut = null;
+			}
+			this.updateChartTimeOut = setTimeout(this.updateChart.bind(this), 10);
 		}
 	}
 	makeGraphics() {
@@ -2796,6 +2803,10 @@ class TimePlotVisual extends PlotVisual {
 		
 	}
 	updateChart() {
+		// Dont update chart if primitive has been deleted
+		// This check needs to be here since updateChart is updated with a timeout 
+		if (! (this.id in connection_array)) return;
+
 		if (this.serieArray == null || this.serieArray.length == 0) {
 			this.setEmptyPlot();
 			return;
@@ -3079,6 +3090,10 @@ class ComparePlotVisual extends PlotVisual {
 		 },200);
 	}
 	updateChart() {
+		// Dont update chart if primitive has been deleted
+		// This check needs to be here since updateChart is updated with a timeout 
+		if (! (this.id in connection_array)) return;
+
 		if (this.serieArray == null || this.serieArray.length == 0 || this.serieArray[0].length === 0) {
 			// The series are not initialized yet
 			this.setEmptyPlot();
@@ -3368,6 +3383,10 @@ class HistoPlotVisual extends PlotVisual {
 		 },200);
 	}
 	updateChart() {
+		// Dont update chart if primitive has been deleted
+		// This check needs to be here since updateChart is updated with a timeout 
+		if (! (this.id in connection_array)) return;
+
 		if (this.serieArray == null) {
 			// The series are not initialized yet
 			this.setEmptyPlot();
@@ -3610,6 +3629,10 @@ class XyPlotVisual extends PlotVisual {
 	}
 	
 	updateChart() {
+		// Dont update chart if primitive has been deleted
+		// This check needs to be here since updateChart is updated with a timeout 
+		if (! (this.id in connection_array)) return;
+
 		if (this.serieArray == null) {
 			// The series are not initialized yet
 			this.setEmptyPlot();
