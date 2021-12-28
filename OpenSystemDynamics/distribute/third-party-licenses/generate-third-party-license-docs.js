87720d536ee888f7e1109178a23f521035ba8436
StochSD team <klirktag@protonmail.com>
2021-12-28 13:34:28 +0100

    WIP: improve-save-on-loaded-file-behaviour

--- OpenSystemDynamics/distribute/third-party-licenses/generate-third-party-license-docs.js
+++ OpenSystemDynamics/distribute/third-party-licenses/generate-third-party-license-docs.js
@@ -276,6 +276,26 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 				LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 				OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 				THE SOFTWARE.`
+			}
+			,
+			{
+				"name": "IDB-Keyval",
+				"links": ["https://github.com/jakearchibald/idb-keyval", "https://github.com/jakearchibald/idb-keyval/blob/main/LICENCE"],
+				"typeoflicense": "Apache License",
+				"license_text": `
+				Copyright 2016, Jake Archibald
+
+				Licensed under the Apache License, Version 2.0 (the "License");
+				you may not use this file except in compliance with the License.
+				You may obtain a copy of the License at
+
+					http://www.apache.org/licenses/LICENSE-2.0
+
+				Unless required by applicable law or agreed to in writing, software
+				distributed under the License is distributed on an "AS IS" BASIS,
+				WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+				See the License for the specific language governing permissions and
+				limitations under the License.`
 
 			}
 ];
