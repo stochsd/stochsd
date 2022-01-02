59209e156e679069187bd73ec4fa508896e35d76
Magnus93 <magnus.ja.gustafsson@gmail.com>
2022-01-02 18:14:47 +0100

    Change theme color and remove extra console.logs

--- MultiSimulationAnalyser/multisimulationanalyser-serviceworker.js
+++ MultiSimulationAnalyser/multisimulationanalyser-serviceworker.js
@@ -1,9 +1,2 @@
-self.addEventListener('install', function(event) {
-    console.log('install event', event);
-});
-
-self.addEventListener('fetch', function(event) {
-    console.log('fetch event', event);
-});
-
-console.log('in service worker');
+self.addEventListener('install', () => {});
+self.addEventListener('fetch', () => {});
