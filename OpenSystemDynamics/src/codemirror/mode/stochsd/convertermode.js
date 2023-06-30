d8543a0b9fcc4da5633a4033cda3b18733c36212
Magnus93 <magnus.ja.gustafsson@gmail.com>
2023-07-01 00:18:22 +0200

    change converter codemirror highlighting

--- OpenSystemDynamics/src/codemirror/mode/stochsd/convertermode.js
+++ OpenSystemDynamics/src/codemirror/mode/stochsd/convertermode.js
@@ -6,9 +6,10 @@ CodeMirror.defineSimpleMode("convertermode", {
   // The start state contains the rules that are intially used
   start: [
       // The regex matches the token, the token property contains the type
-      {regex: /^(\t| )*\d+(\.\d+)?(\t| )*(?=,)/, token: "x"}, 
-      {regex:  /(\t| )*\d+(\.\d+)?(\t| )*(?=;)/, token: "y"}, 
-      {regex:  /(\t| )*\d+(\.\d+)?(\t| )*$/, token: "y"}, 
+      {regex:  /\d*(\.\d+)?(\t| )*(\t| )*,(\t| )*\d*(\.\d+)?/, token: "point"}, 
+      // {regex: /^(\t| )*\d+(\.\d+)?(\t| )*(?=,)/, token: "x"}, 
+      // {regex:  /(\t| )*\d+(\.\d+)?(\t| )*(?=;)/, token: "y"}, 
+      // {regex:  /(\t| )*\d+(\.\d+)?(\t| )*$/, token: "y"}, 
   ],
   // The meta property contains global information about the mode. It
   // can contain properties like lineComment, which are supported by
