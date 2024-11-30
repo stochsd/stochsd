1a93aed9616a5cf3b0336fdde2bbe78c8cf8ce1b
Magnus93 <magnus.ja.gustafsson@gmail.com>
2024-11-30 09:00:16 +0100

    Change file structure in OPenSystemDynamics

--- OpenSystemDynamics/distribute/gulpfile.js
+++ OpenSystemDynamics/distribute/gulpfile.js
@@ -1,13 +1,13 @@
 const gulp = require('gulp');
 const useref = require('gulp-useref');
-gulp.task('default' , function() {
-	gulp.src('../src/index.html')
-	.pipe(useref())
-	.pipe(gulp.dest('build'));
-	
+gulp.task('default', function () {
+	gulp.src('../index.html')
+		.pipe(useref())
+		.pipe(gulp.dest('build'));
+
 	gulp.src('../src/third-party-licenses.html')
-	.pipe(gulp.dest('build'));
-	
+		.pipe(gulp.dest('build'));
+
 	gulp.src('../src/graphics/**')
-	.pipe(gulp.dest('build/graphics'));
+		.pipe(gulp.dest('build/graphics'));
 });
